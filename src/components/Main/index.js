a94534cf06969eec4ef76511ab5e19585d8a2742
adilet <adiletkub@gmail.com>
2019-10-07 15:31:01 +0600

    finished Main  with styles

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './styles.scss';
+import mainImages from '../../assets/images/gaming-pc-alt-3.jpg'
 
 const Main = () => {
 
@@ -8,12 +9,21 @@ const Main = () => {
 		<div className='main'>
 			<div className='container'>
 				<div className='main-wrapper'>
-					<p>
-					</p>
-					<img />
-					<div>
-						
+					<h1 className='main__title'>
+						Игровые компьютеры и системные блоки
+					</h1>
+					<div className='main-image-wrapper'>
+						<img
+							src={mainImages}
+							alt="PC"
+							className='main__image' />
+						<div className='main-text-wrapper'>
+							<span className='main__text'>
+								Когда в одном системном блоке объединяются все самые мощные технологии, присущие центральным и графическим процессорам, это и есть энергия компьютеров HYPERUPGRADE
+							</span>
+						</div>
 					</div>
+
 				</div>
 			</div>
 		</div>
