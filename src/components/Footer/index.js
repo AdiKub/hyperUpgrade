5c337a576665a8c559624957e83141914b041252
adilet <adiletkub@gmail.com>
2019-10-08 13:33:58 +0600

    added rgb play effect fot footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.scss';
 
 const Footer = () => {
+	const [colorArray, setColor] = useState(['255,23,23,1', '55,255,71,1', '48,125,255,1', '255,40,254,1']) 
+
+	const firstToLast = () => {
+		const arr2 = colorArray.splice(-1,1)
+		const ppd = arr2.concat(...colorArray) 
+		setColor(ppd)
+	}
+	setTimeout(()=> firstToLast(), 1000)
 	return (
-		<div className='footer'>
+		<div
+			style={{
+				background: `linear-gradient(90deg, rgba(${colorArray[0]}) 0%, rgba(${colorArray[1]}) 33%, rgba(${colorArray[2]}) 66%, rgba(${colorArray[3]}) 100%)`,
+				transition: '1000ms',
+			}}
+			className='footer'>
 			<div className='container'>
 				<div className='footer-wrapper'>
 					<h3>
--- src/components/MainProductList/index.js
+++ src/components/MainProductList/index.js
@@ -9,31 +9,38 @@ import extreme from '../../assets/images/categoryImages/04.jpg'
 const MainProductList = () => {
 	const pcArr = [basic, optimal, prof, extreme]
 	const categoryArr = [
-		'Недорогие игровые компьютеры', 'Идеальные игровые компьютеры',
-		'Профессиональные игровые компьютеры', 'Самые мощные игровые компьютеры'
+		'Игровые', 'Рабочие'
+	]
+	const categoryPCArr = [
+		'Недорогие', 'Идеальные',
+		'Профессиональные ', 'Самые мощные'
 	]
 
 	return (
 		<div className='main-product-list'>
 			<div className='container'>
-				<div className='main-product-list-wrapper'>
-					<h2 className='main-product-list__title'>
-						Игровые компьютеры
-					</h2>
-					<div className='main-product-list-categories'>
-						{categoryArr.map((category, index) => (
-							<div key={category}>
-								<div className='main-product-list-category'>
-									<span className='main-product-list-category__name'>
-										{category}
-									</span>
-									<img src={pcArr[index]} className='main-product-list-category__image' alt='pc' />
-									<a className='main-product-list-category__link'> подробнее </a>
+				{categoryArr.map((categoryArr) => (
+					<div 
+						key={categoryArr} 
+						className='main-product-list-wrapper'>
+						<h2 className='main-product-list__title'>
+							{categoryArr} компьютеры
+						</h2>
+						<div className='main-product-list-categories'>
+							{categoryPCArr.map((category, index) => (
+								<div key={category}>
+									<div className='main-product-list-category'>
+										<span className='main-product-list-category__name'>
+											{category +' '+categoryArr}
+										</span>
+										<img src={pcArr[index]} className='main-product-list-category__image' alt='pc' />
+										<a href='dfdfd.dfdf' className='main-product-list-category__link'> подробнее </a>
+									</div>
 								</div>
-							</div>
-						))}
+							))}
+						</div>
 					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	)
