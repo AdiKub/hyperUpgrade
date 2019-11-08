b99a69a076207d7c80a810e906e8dffbf88ac8c1
AdiKub <adiletkub@gmail.com>
2019-11-08 15:19:13 +0600

    added Link for CartItem

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 import './styles.scss';
 import pc_01 from '../../assets/images/categoryImages/01.jpg'
@@ -12,14 +13,22 @@ const CartItem = (props) => {
     <div 
       key={cartPc.pc._id} 
       className='product-cart-table-items product-cart-table-line'>
-      <div className='product-cart-table__card product-cart-table__product'>
-        <div >
-          <img
-            src={pc_01}
-            alt='pc'
-            className='product-cart-table__card_img' />
-        </div>
-        <h4 className='product-cart-table__card__name'> {cartPc.pc.pcname} </h4>
+      <div
+        className='product-cart-table__card product-cart-table__product'>
+        <Link to='/detail'>
+          <div
+            className='product-cart-table__product__title'
+            onClick={() => localStorage.setItem( 'selectedPC' , cartPc.pc._id)} >
+            <img
+              src={pc_01}
+              alt='pc'
+              className='product-cart-table__card_img' />
+            <h4
+              className='product-cart-table__card__name'>
+              {cartPc.pc.pcname}
+            </h4>
+          </div>
+        </Link>
       </div>
       <div className='product-cart-table-header__name product-cart-table__available'>
         <span > {cartPc.pc.inStock ? 'in stock' : 'Coming Soon'}  </span>
